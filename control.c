386deaef1b536204f6c6b6d70114c20dd98d7ffe
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-03-22 09:56:51 -0500

    Computes the step signal

--- control.c
+++ control.c
@@ -4,11 +4,28 @@
 
 void GetStepVector(double * ctrlSignal, double acqTime, double sTime, double stepTime)
 {
-	long nSamples = lround(acqTime/sTime + 1);
-	long prevSamples = lround(stepTime/sTime + 1);
+	long nSamples = lround(acqTime/sTime) + 1;
+	long prevSamples = lround(stepTime/sTime) + 1;
 	long i;
+	
 	// fill the step signal
 	for(i = 0; i < nSamples; i++)
 		ctrlSignal[i] = i < prevSamples?0:1;		
 }
+
+void GetTimeVector(double * timeVector, double acqTime, double sTime)
+{
+	long nSamples = lround(acqTime/sTime) + 1;
+	long i;
+	double acu = 0.0;
+	
+	// fill the time array
+	for(i = 0; i < nSamples; i++)
+	{	
+		acu += sTime;
+		timeVector[i] = acu; 	
+	}
+}
+
+
 
