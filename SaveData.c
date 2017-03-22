93f4530f7c23759a730403c3126ec5bf6d1caa3b
Julio César Echeverri Marulanda <julio.em7@gmail.com>
2017-03-22 09:57:52 -0500

    Function that stores the data into a text file

---
+++ SaveData.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <SaveData.h>
+
+
+int SaveData(char * filename, 
+			 double *timeArray,
+			 double *dataArray, 
+			 double *ctrlSignal, 
+			 int dataLength)
+{
+	
+	FILE *fp;
+	int i;
+	
+	fp = fopen(filename, "w");	
+	
+	if(fp == NULL) //Something goes wrong
+		return 1;
+		
+	/*Writes plant data into file*/
+	/*The format is: timeArray - ctrlSignal - dataArray*/	
+	for(i = 0; i < dataLength; i++)
+		fprintf(fp,"%f\t%f\%f\n",timeArray[i], ctrlSignal[i], dataArray[i]);	
+		
+	fclose(fp);
+	return 0;
+}			 
+
+
+
+
+
+
